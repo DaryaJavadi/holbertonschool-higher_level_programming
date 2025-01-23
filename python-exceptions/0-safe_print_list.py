3f8067d302f3f0023cb4eb8fc91bfe351901eb20
Darya Javadi <darya.javadi15@gmail.com>
2025-01-23 22:53:18 +0400

    exception

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -7,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             count += 1
         except IndexError:
-            pass
+            break
     print()
     return count
