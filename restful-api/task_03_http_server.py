4b5fa18c7073b9bd4d75cfb21f4d86002cb73bad
Darya Javadi <darya.javadi15@gmail.com>
2025-02-21 17:13:36 +0400

    api

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,31 +1,31 @@
-import http.server
 import json
+import http.server
 
-class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/":
+        if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
-        
-        elif self.path == "/data":
+            self.wfile.write('Hello, this is a simple API!'.encode('utf-8'))
+
+        elif self.path == '/data':
             self.send_response(200)
-            self.send_header("Content-type", "application/json")
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode())  # Convert dict to JSON string and encode to bytes
+            self.wfile.write(json.dumps(data).encode('utf-8'))
 
-        elif self.path == "/status":
+        elif self.path == '/status':
             self.send_response(200)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"OK")
+            self.wfile.write('OK'.encode('utf-8'))
 
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain")
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write('Endpoint not found'.encode('utf-8'))
 
-http.server.HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler)
+http.server.HTTPServer(('', 8000), SimpleAPIHandler).serve_forever()
